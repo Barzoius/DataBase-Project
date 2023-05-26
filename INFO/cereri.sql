9d84a6ad200593a8440d737cf027a196e6d046ba
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-05-26 08:55:46 -1100

    Update cereri.sql

--- INFO/cereri.sql
+++ INFO/cereri.sql
@@ -1,3 +1,13 @@
+select j.id_job, j.titlu_job, e.id_echipa, e.nume_echipa,l.id_locatie, INITCAP(l.oras)	
+from job j, echipa e, locatie l
+where j.salariu_min > (select a.salariu
+    				   from angajat a
+    				   where a.id_job = j.id_job
+    				   and a.id_echipa = e.id_echipa
+    				   and e.id_locatie = l.id_locatie)
+
+----------------(2)----------------
+
 SELECT LOWER(t.NUME_TARA), v.ID_VANZATOR, v.NUME_VANZATOR, m.NR_MAGAZINE,  v.VENIT_TOTAL_ADUS, 
 CASE 
 	WHEN (m.NR_MAGAZINE/t.PRET_DE_EXPORT) > t.PRET_DE_EXPORT THEN INITCAP('ACEST VANAZATOR NE FACE PROFIT.')
@@ -10,64 +20,38 @@ AND v.ID_VANZATOR = m.ID_VANZATOR
 AND v.VENIT_TOTAL_ADUS >= 1500000
 ORDER BY m.NR_MAGAZINE;
 
-SELECT a.ID_ANGAJAT, CONCAT(a.NUME, CONCAT(' ', a.PRENUME)) NUME_COMPLET, a.ID_JOB 
-FROM (SELECT AVG(SALARIU_MAX) AS SALARIU_MAX_MEDIU FROM JOB) SMD, ANGAJAT a
-WHERE a.SALARIU > SMD.SALARIU_MAX_MEDIU;
+----------------(3)----------------
 
-SELECT LAST_DAY(p.DEADLINE), ROUND(MONTHS_BETWEEN(m.DATA_SFARSIT_FAB, m.DATA_INCEPUT_FAB)) PERIOADA_FAB ,
+select g.id_generatie, min(m.pret), max(m.pret) 
+from model m, generatie g, consola c
+where  data_inceput_fab > any(select data_emitere_cerere 
+    					   from cerere ce, consola c, procesor pr, producator pro
+    					   where ce.id_consola = c.id_consola
+    					   and ce.id_procesor = pr.id_procesor
+    		               and ce.id_producator = pro.id_producator)
+and m.id_consola =  c.id_consola
+and c.id_generatie = g.id_generatie
+group by g.id_generatie
+having max(m.pret) > 99;
+
+----------------(4)----------------
+
+SELECT a.nume, a.prenume, LAST_DAY(p.DEADLINE) dead, ROUND(MONTHS_BETWEEN(m.DATA_SFARSIT_FAB, m.DATA_INCEPUT_FAB)) PERIOADA_FAB ,
     p.ID_ECHIPA, CONCAT(m.ID_CONSOLA, CONCAT(' ', m.ID_MODEL)) PROIECT
-FROM MODEL m, ECHIPA e, PLAN p
-WHERE p.ID_ECHIPA = e.ID_ECHIPA;
+FROM MODEL m, ECHIPA e, PLAN p, (select avg(salariu) as sal_avg from angajat) sal_avg_ang, angajat a
+WHERE p.ID_ECHIPA = e.ID_ECHIPA
+and e.id_echipa = a.id_echipa
+and p.id_model = m.id_model
+and sal_avg_ang.sal_avg < a.salariu
+and last_day(p.deadline) < m.data_inceput_fab;
+
+----------------(5)----------------
 
 SELECT v.NUME_VANZATOR || ' e un magazin electronic' AS E_MAGAZIN
 FROM MAGAZIN m, VANZATOR v
 WHERE NVL(DECODE(NR_MAGAZINE, 0, -1), 12) = -1
 AND m.ID_VANZATOR = v.ID_VANZATOR;
-
-select INITCAP(a.NUME), INITCAP(a.PRENUME), l.COD_POSTAL, l.ORAS, t.NUME_TARA, a.ID_jOB
-from ANGAJAT a, ECHIPA e, LOCATIE l, tara t
-where a.ID_ECHIPA = e.ID_ECHIPA
-and e.ID_LOCATIE = l.ID_LOCATIE
-and t.ID_TARA = l.ID_TARA
-and SUBSTR(l.COD_POSTAL, -3) = '141';
-
-
-select INITCAP(NUME), INITCAP(PRENUME), ID_JOB
-from ANGAJAT 
-where ID_ECHIPA in (select ID_ECHIPA
-    					from ECHIPA 
-    					where ID_LOCATIE in (select ID_LOCATIE
-    										from LOCATIE
-    										where SUBSTR(COD_POSTAL, -3) = '141'));
-										
-										
-								
-select j.ZILE_DE_LUCRU, SUM(a.SALARIU) as SAL_TOTAL, a.ID_JOB
-from ANGAJAT a, JOB j
-where a.ID_JOB = j.ID_JOB
-group by j.ZILE_DE_LUCRU, a.ID_JOB
-having SUM(a.SALARIU)  > (  select avg(j.SALARIU_MAX)
-    						from JOB j); !!!!!!!!!!!!!!!!!!!!!	
-						
-				
-select SUM(m.PRET), l.NR_UNITATI, c.DATA_LANSARE
-from MODEL m, LOT l, CONSOLA c
-where c.ID_CONSOLA = m.ID_CONSOLA
-and m.ID_MODEL = l.ID_MODEL
-group by l.NR_UNITATI, c.DATA_LANSARE
-having SUM(m.PRET)*l.NR_UNITATI > (select avg(l.VENITURI_ASTEPTATE)
-    				   from LOT l);		
-				   
-			
-with top_jobs as(
-    	select TITLU_JOB, SALARIU_MAX, ID_JOB
-    	from JOB
-    	where SALARIU_MIN > 2500
-    	order by SALARIU_MAX)
-
-select aux.TITLU_JOB, a.NUME, a.PRENUME
-from ANGAJAT a, top_jobs aux
-where aux.ID_JOB = a.ID_JOB;		
+	
 
 
 ------------------------------------------------(13)-------------------------------------------------------
